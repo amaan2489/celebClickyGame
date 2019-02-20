4173795e3f1ec97dcfa2837b507a89b92c069f18
Amaan Javed <amaan.javed@amaanjaved-ltm.internal.salesforce.com>
2019-02-19 18:16:58 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Celebrity-Clicky-Game
+
+Built on React, this game is memory based clicks on celebrity who will appear randomly on the screen.
+
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import MainGameContainer from "./components/MainGameContainer";
+import "./App.css";
+
+
+const App = () => (
+  <div className="container-fluid mainContainer">
+    <MainGameContainer /> 
+  </div>
+);
+
+export default App;
